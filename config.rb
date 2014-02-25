e89c9752b42cc6c2b042625fa17341d76bde917a
Evan Walsh <evan@massivedanger.com>
2014-02-25 17:33:09 -0500

    When is Arbor Day?

---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'bootstrap-sass'
+require 'compass-normalize'
+
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style = :compressed
+preferred_syntax = :sass
